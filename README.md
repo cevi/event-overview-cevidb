28973b5f60c4db9cda29e0ce75ceb4ec67b6d791
Cyrill Püntener <wp99cp@gmx.net>
2022-11-16 13:30:08 +0100

    proof of concept

---
+++ README.md
@@ -0,0 +1,15 @@
+# CeviDB Event Overview
+
+This is a small web application to display events from the [CeviDB](https://cevidb.ch) in a nice way.
+
+The application is written using NextJS.
+
+## Development
+
+To start the development server, run:
+
+```bash
+CEVI_DB_TOKEN=xxx npm run dev
+```
+
+Where `xxx` is the API token for the CeviDB API with the `Anlässe dieser und der darunterliegenden Ebenen` scope.
---
+++ pages/index.js
@@ -0,0 +1,64 @@
+import Layout from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+async function getEventsData() {
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    // load token from environment variable
+    const token = process.env.CEVI_DB_TOKEN;
+
+    const res = await fetch("https://demo.hitobito.com/groups/1/events.json?token=" + token, requestOptions)
+    const jsonResult = await res.json();
+    return jsonResult.events;
+
+
+}
+
+// This value is considered fresh for ten seconds (s-maxage=10).
+// If a request is repeated within the next 10 seconds, the previously
+// cached value will still be fresh. If the request is repeated before 59 seconds,
+// the cached value will be stale but still render (stale-while-revalidate=59).
+//
+// In the background, a revalidation request will be made to populate the cache
+// with a fresh value. If you refresh the page, you will see the new value.
+export async function getServerSideProps({req, res}) {
+
+    res.setHeader(
+        'Cache-Control',
+        'public, s-maxage=10, stale-while-revalidate=59'
+    )
+
+    const allDBEvents = await getEventsData();
+    return {
+        props: {
+            "allDBEvents": allDBEvents
+        },
+    };
+}
+
+export default function Home({allDBEvents}) {
+    return (
+        <Layout home>
+
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Anlässe</h2>
+                <ul className={utilStyles.list}>
+                    {allDBEvents.map(event => (
+                        <li className={utilStyles.listItem} key={event.id}>
+                            {event.name} <br/> {event.description} <br/>
+                            Anmeldungen: {event.participant_count} / {event.maximum_participants}
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </Layout>
+    );
+}
