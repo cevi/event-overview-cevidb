0fb659ec293fb66749e1b6521d84c63b1832cad6
Cyrill Püntener <wp99cp@gmx.net>
2023-01-16 11:54:16 +0100

    include date in table

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
+type DBEvent_Date = {
+    start_at: string,
+    finish_at: string,
+    location: string
+    label: string
+}
+
 type DBEvent = {
     id: number,
     name: string,
+    motto: string,
+    location: string,
     description: string,
     participant_count: number,
     maximum_participants: number,
+    dates: DBEvent_Date[],
 }
 
 async function getEventsData(): Promise<DBEvent[]> {
@@ -23,10 +33,43 @@ async function getEventsData(): Promise<DBEvent[]> {
     // load token from environment variable
     const token = process.env.CEVI_DB_TOKEN;
 
-    const res = await fetch("https://demo.hitobito.com/groups/1/events.json?token=" + token, requestOptions)
-    const jsonResult = await res.json();
-    return jsonResult.events;
+    // events and courses must be retrieved separately using different endpoints
+    const api_endpoints = [
+        "https://demo.hitobito.com/groups/1/events.json?token=" + token,
+        "https://demo.hitobito.com/groups/1/events/course.json?token=" + token
+    ]
 
+    const events: DBEvent[] = [];
+
+    for (const api_endpoint of api_endpoints) {
+        const response = await fetch(api_endpoint, requestOptions);
+        const data = await response.json();
+
+        // TODO: paging is not implemented yet;
+        //       only events from the first page are loaded
+
+        // extract date of event
+        data.events.map((event: any) => {
+
+            const date_ids = event.links.dates;
+            const date_objs = data.linked.event_dates;
+
+            // find all associated dates objects
+            event.dates = date_objs
+                .filter((date_obj: any) => date_ids.includes(date_obj.id))
+                .map((date_obj: any) => {
+                    date_obj.location ||= "unbekannt";
+                    return date_obj as DBEvent_Date;
+                });
+
+            event.dates ||= []; // if dates is undefined, set it to an empty array
+            return event as DBEvent;
+        });
+
+        events.push(...data.events);
+    }
+
+    return events;
 
 }
 
@@ -52,6 +95,50 @@ export async function getServerSideProps({req, res}: { req: any, res: any }) {
     };
 }
 
+function date_formatter(date: Date, extras = {}): string {
+    let format: Intl.DateTimeFormatOptions = {day: '2-digit'};
+    format = {...format, ...extras};
+    return date.toLocaleDateString('de-CH', format);
+}
+
+function time_formatter(date: Date, extras = {}): string {
+
+    let format: Intl.DateTimeFormatOptions = {day: '2-digit', hour: '2-digit', minute: '2-digit'};
+    format = {...format, ...extras};
+    return date.toLocaleDateString('de-CH', format);
+}
+
+function dbEventDateToString(date: DBEvent_Date) {
+
+    const event_date: any = date;
+    event_date.start_at = new Date(event_date.start_at);
+    event_date.finish_at = new Date(event_date.finish_at);
+
+    if (date.start_at === date.finish_at) {
+        return `${date_formatter(event_date.start_at)} (Ort: ${date.location})`;
+    }
+
+    let formatter = time_formatter;
+    if (event_date.start_at.toLocaleTimeString() === event_date.finish_at.toLocaleTimeString()) {
+        formatter = date_formatter;
+    }
+
+    let extras = {};
+    if (event_date.start_at.month !== event_date.finish_at.month) {
+        extras = {...extras, month: '2-digit'};
+    }
+
+    if (event_date.start_at.year !== event_date.finish_at.year) {
+        extras = {...extras, year: 'numeric'};
+    }
+
+    return (<span>
+        <b>{event_date.label}:</b>
+        <br />
+        {formatter(event_date.start_at, extras)} -
+        {formatter(event_date.finish_at, {...extras, month: '2-digit', year: 'numeric'})} <br />
+    </span>);
+}
 
 export default function Home({allDBEvents}: { allDBEvents: DBEvent[] }) {
     return (
@@ -65,22 +152,35 @@ export default function Home({allDBEvents}: { allDBEvents: DBEvent[] }) {
                     <tr>
                         <th scope="col" className="px-6 py-3">Name</th>
                         <th scope="col" className="px-6 py-3">Beschreibung</th>
+                        <th scope="col" className="px-6 py-3">Daten</th>
                         <th scope="col" className="px-6 py-3">Anmeldungen</th>
                     </tr>
                     </thead>
 
                     <tbody>
-                    {allDBEvents.map((event: DBEvent) => (
+                    {allDBEvents.slice(0, 15).map((event: DBEvent) => (
                         <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
                             key={event.id}>
                             <td className="px-6 py-4">{event.name}</td>
                             <td className="px-6 py-4">{event.description}</td>
+                            <td className="px-6 py-4">{
+                                event.dates.map((date: DBEvent_Date) => (
+                                    <div key={date.label}>
+                                        {dbEventDateToString(date)}
+                                    </div>
+                                ))
+                            }</td>
                             <td className="px-6 py-4 text-center">{event.participant_count} / {event.maximum_participants}</td>
                         </tr>
                     ))}
+
                     </tbody>
 
                 </table>
+
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5">
+                    Mehr Events/Lager anzeigen
+                </button>
 
             </section>
         </Layout>
