001eb313e5dc0252bc957994c6b0787429c32ffd
Patrick Uhlmann <github@uhlme.ch>
2026-03-23 23:34:40 +0100

    fix startup problem with empty linked in hitobito event page

--- backend/src/main/java/ch/cevi/db/adapter/hitobito/HitobitoEventPage.java
+++ backend/src/main/java/ch/cevi/db/adapter/hitobito/HitobitoEventPage.java
@@ -5,6 +5,6 @@ import java.util.Objects;
 public record HitobitoEventPage(int current_page, int total_pages, String next_page_link, HitobitoEvent[] events, HitobitoLinked linked) {
     public HitobitoEventPage {
         Objects.requireNonNull(events);
-        Objects.requireNonNull(linked);
+        if (linked == null) linked = new HitobitoLinked(new HitobitoEventDate[0], new HitobitoGroup[0], null);
     }
 }
