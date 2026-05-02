558e22197eb92916e925542027138eb7601c168d
Patrick Uhlmann <github@uhlme.ch>
2026-05-02 13:36:52 +0200

    feat(backend): allow filtering by multiple kursarten

--- backend/src/test/java/ch/cevi/db/adapter/EventFilterTest.java
+++ backend/src/test/java/ch/cevi/db/adapter/EventFilterTest.java
@@ -4,10 +4,11 @@ import ch.cevi.db.adapter.fixtures.CeviEventFixture;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class EventFilterTest {
+class EventFilterTest {
     @Test
     void should_filter_available_seats() {
         var ceviEvent = CeviEventFixture.ceviEvent();
@@ -42,6 +43,47 @@ public class EventFilterTest {
         var eventOverbooked = ceviEvent.withMaximumParticipants(ceviEvent.participantsCount());
         // match as there are already too many participants
         assertThat(sut.match(eventOverbooked)).isTrue();
+    }
+
+    @Test
+    void should_not_filter_kursarten_when_null_or_empty() {
+        var event = CeviEventFixture.ceviEvent().withKind("J+S-Leiter*innenkurs LS/T Jugendliche");
+
+        assertThat(EventFilter.emptyFilter().match(event)).isTrue();
+        assertThat(EventFilter.emptyFilter().withKursarten(List.of()).match(event)).isTrue();
+    }
+
+    @Test
+    void should_filter_by_single_kursart() {
+        var matching = CeviEventFixture.ceviEvent().withKind("J+S-Leiter*innenkurs LS/T Jugendliche");
+        var nonMatching = CeviEventFixture.ceviEvent().withKind("J+S-Coachkurs LS/T");
+        var sut = EventFilter.emptyFilter().withKursarten(List.of("J+S-Leiter*innenkurs LS/T Jugendliche"));
+
+        assertThat(sut.match(matching)).isTrue();
+        assertThat(sut.match(nonMatching)).isFalse();
+    }
+
+    @Test
+    void should_filter_by_multiple_kursarten() {
+        var event1 = CeviEventFixture.ceviEvent().withKind("J+S-Leiter*innenkurs LS/T Jugendliche");
+        var event2 = CeviEventFixture.ceviEvent().withKind("J+S-Einführungskurs LS/T Kinder");
+        var nonMatching = CeviEventFixture.ceviEvent().withKind("J+S-Coachkurs LS/T");
+        var sut = EventFilter.emptyFilter().withKursarten(List.of(
+                "J+S-Leiter*innenkurs LS/T Jugendliche",
+                "J+S-Einführungskurs LS/T Kinder"
+        ));
+
+        assertThat(sut.match(event1)).isTrue();
+        assertThat(sut.match(event2)).isTrue();
+        assertThat(sut.match(nonMatching)).isFalse();
+    }
+
+    @Test
+    void should_filter_kursarten_case_insensitive() {
+        var event = CeviEventFixture.ceviEvent().withKind("J+S-Leiter*innenkurs LS/T Jugendliche");
+        var sut = EventFilter.emptyFilter().withKursarten(List.of("j+s-leiter*innenkurs ls/t jugendliche"));
+
+        assertThat(sut.match(event)).isTrue();
     }
 
     @Test
