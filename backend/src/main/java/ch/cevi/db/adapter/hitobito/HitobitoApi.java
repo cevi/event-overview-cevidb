5ebfae5363ebb73537706ba2e5cdc64e9299ec0e
Patrick Uhlmann <github@uhlme.ch>
2026-05-03 10:04:07 +0200

    chore: fix MappingJackson2HttpMessageConverter deprecation error

--- backend/src/main/java/ch/cevi/db/adapter/hitobito/HitobitoApi.java
+++ backend/src/main/java/ch/cevi/db/adapter/hitobito/HitobitoApi.java
@@ -2,10 +2,8 @@ package ch.cevi.db.adapter.hitobito;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,6 +11,8 @@ import java.util.Objects;
 
 class HitobitoApi implements HitobitoApiProvider {
     Logger logger = LoggerFactory.getLogger(HitobitoApi.class);
+
+    private final RestClient restClient = RestClient.create();
 
     private final String firstEventPageUrl;
     private final String firstCoursePageUrl;
@@ -36,15 +36,10 @@ class HitobitoApi implements HitobitoApiProvider {
     private List<HitobitoEventPage> discoverEventPages(String startUrl) {
         List<HitobitoEventPage> pages = new LinkedList<>();
 
-        var converter = new MappingJackson2HttpMessageConverter();
-        converter.setDefaultCharset(StandardCharsets.UTF_8);
-        var restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().addFirst(converter);
-
         String nextPageUrl = startUrl;
         while (nextPageUrl != null) {
             long startMillis = System.currentTimeMillis();
-            var page = restTemplate.getForObject(nextPageUrl, HitobitoEventPage.class);
+            var page = restClient.get().uri(nextPageUrl).retrieve().body(HitobitoEventPage.class);
             logger.atDebug().log("Request to Cevi.db took {}ms", System.currentTimeMillis() - startMillis);
             pages.add(page);
             nextPageUrl = page.next_page_link();
